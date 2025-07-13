00c07dfdd277bca434ee4c446f90ac41e9719919
modtherhub <moibhub@gmail.com>
2025-07-13 23:17:18 +0400

    create and sok task 2

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns to display
+    list_filter = ('publication_year', 'author')            # Filters sidebar
+    search_fields = ('title', 'author')                      # Search box
+
+
+admin.site.register(Book)
