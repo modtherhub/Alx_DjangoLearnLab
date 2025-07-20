3545b9e46b48f705ad117895c971ef4fb4a21121
modtherhub <moibhub@gmail.com>
2025-07-20 15:43:42 +0400

    works on task 02

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library    # ✅ <- This is required for the check
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
 
 # Function-based view to list all books
 def list_books(request):
@@ -13,3 +19,17 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto login after registration
+            return redirect('home')  # Redirect to home or wherever
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+def home(request):
+    return HttpResponse("<h1>Welcome to the Library App!</h1><p><a href='/books/'>View Books</a></p>")
