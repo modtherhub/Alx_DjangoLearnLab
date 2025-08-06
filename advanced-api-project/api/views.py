671b4503314482e88857047ace8d379e635f4b89
modtherhub <moibhub@gmail.com>
2025-08-07 00:21:48 +0400

    week-13 task-02 fix-02

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework as filters 
+from django_filters.rest_framework import DjangoFilterBackend
 
 # عرض جميع الكتب (List)
 class BookListView(generics.ListAPIView):
