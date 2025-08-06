e1d041fbae6ec1bf0a989cdf82eea6b0d270754f
modtherhub <moibhub@gmail.com>
2025-08-06 20:04:21 +0400

    week-11 task-02

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,6 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
+
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(label='Example Field', max_length=100)
