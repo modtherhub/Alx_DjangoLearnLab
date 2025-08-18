877feff38f5e1c4a1ccfaa8de1a7c826d6f0f9d3
moubhub <moibhub@gmail.com>
2025-08-18 06:31:59 +0400

    week-14 task-04 fix-02

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Post
 from django import forms
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     tags = forms.CharField(required=False, help_text="Enter tags separated by commas")
@@ -9,6 +10,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  
+        }
 
     def save(self, commit=True):
         post = super().save(commit=False)
