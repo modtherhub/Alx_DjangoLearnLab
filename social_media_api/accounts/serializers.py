9575330ce2bb66231762b4a37decc709e472f380
moubhub <moibhub@gmail.com>
2025-08-21 16:38:02 +0400

    week-15 task-00 fix-02

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -19,8 +19,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        User = get_user_model()
-        user = User.objects.create_user(**validated_data)  # ✅ هنا
+        user = get_user_model().objects.create_user(**validated_data)  # ✅ هنا
         from rest_framework.authtoken.models import Token
         Token.objects.create(user=user)
         return user
